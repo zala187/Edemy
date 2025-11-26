1d5d9628dd0bc6c67864270fa59d5ee209cb9c5a
zalamanish18 <zalamanish8595@gmail.com>
2025-11-26 21:00:38 +0530

    com

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Edemy/">
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
   <AppContextProvider>
     <App />
